85a6dca091abc29ee824c660ac2cff3dc948ad1f
Jayakrishna20 <jayakrishnan2041@gmail.com>
2022-11-18 20:35:53 +0530

    song card component added

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,8 +4,10 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i }) => {
-  const activeSong = "Test";
+const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
+  const handlePauseClick = () => {};
+
+  const handlePlayClick = () => {};
 
   return (
     <div
@@ -21,7 +23,33 @@ const SongCard = ({ song, i }) => {
                         ? "flex bg-black bg-opacity-70"
                         : "hidden"
                     }`}
-        ></div>
+        >
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
+        </div>
+        <img src={song.images?.coverart} alt="song_img" />
+      </div>
+
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0]?.adamid}`
+                : "/top-artists"
+            }
+          >
+            {song.subtitle}
+          </Link>
+        </p>
       </div>
     </div>
   );
