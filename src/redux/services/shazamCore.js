af84ac0da2eac1527b6cc5b06d26dd72ec586c67
Jayakrishna20 <jayakrishnan2041@gmail.com>
2022-11-18 14:20:15 +0530

    rapid api data fetched and loader,Error section added

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "66cfad77e1mshdc712596bcdcc30p17fdbbjsnf028f502c467"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
